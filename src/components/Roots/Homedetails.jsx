72214d30135f3721b1e5eb3681d9a7c6b02d5610
Nafis Ahamed <nafisahamed14@gmail.com>
2024-04-26 15:48:55 +0600

    unique array algorithm

--- src/components/Roots/Homedetails.jsx
+++ src/components/Roots/Homedetails.jsx
@@ -1,57 +1,58 @@
+const Homedetails = ({ item, setItems, items }) => {
+  const { email, _id } = item;
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5006/item/${id}`, {
+      method: "DELETE"
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const reamining = items.filter((it) => it._id !== id);
+        console.log(reamining);
+        setItems(reamining);
+      });
+  };
+  const updateButton = (id) => {
+    console.log("hellow");
+    const updateuser = { email: "nafis ahamed" };
+    fetch(`http://localhost:5006/item/${id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(updateuser)
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          alert("user updated successfully");
+        }
 
+        //  location.reload()
 
+           
 
-
-const Homedetails = ({item,setItems ,items}) => {
-
-    const {email, _id} = item
-    const handleDelete = id =>{
-        fetch(`http://localhost:5006/item/${id}`,{
-            method: "DELETE"
-          })
-          .then(res => res.json())
-          .then(data=>{
-            const reamining = items.filter(it => it._id !== id)
-         console.log(reamining);
-         setItems(reamining)
-          })
-         
-    }
-    const updateButton = id =>{
-        console.log('hellow');
-        const updateuser = {email:"nafis ahamed"}
-        fetch(`http://localhost:5006/item/${id}`,{
-            method: 'PUT',
-            headers:{
-                'content-type':'application/json'
-            },
-            body: JSON.stringify(updateuser)
-          })
-          .then(res => res.json())
-          .then(data => {
-             console.log(data)
-             if(data.modifiedCount > 0){
-                alert('user updated successfully')
-             }
-             
-             location.reload()
-            
-          })
-
-        
-        
-    }
-    return (
-        <div>
-
-            <div className="mt-2 flex gap-4">
-            <h2 className="text-2xl text-blue-700">{email}</h2>
-            <button onClick={()=>handleDelete(_id)} className="btn btn-danger ">X</button>
-            <button onClick={()=>updateButton(_id)} className="btn btn-danger ">U</button>
-            </div>
-            
-        </div>
-    );
+        // const array = items.filter(
+        //   (obj, index, self) =>
+        //     index === self.findIndex((t) => t.email === obj.email)
+        // );
+        // console.log(array);
+      });
+     
+  };
+  return (
+    <div>
+      <div className="mt-2 flex gap-4">
+        <h2 className="text-2xl text-blue-700">{email}</h2>
+        <button onClick={() => handleDelete(_id)} className="btn btn-danger ">
+          X
+        </button>
+        <button onClick={() => updateButton(_id)} className="btn btn-danger ">
+          U
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Homedetails;
