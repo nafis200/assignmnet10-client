8147e455c61cf7897480e041722dd9b23185c51d
Nafis Ahamed <nafisahamed14@gmail.com>
2024-04-26 18:36:03 +0600

    form make done

--- src/components/tourist/Addinfor.jsx
+++ src/components/tourist/Addinfor.jsx
@@ -20,26 +20,152 @@ const Addinfor = () => {
       <h2 className="lg:text-2xl md:text-xl text-center font-bold">Add spot</h2>
 
       <form className="card-body">
+        {/* 1st input */}
         <div className="lg:flex md:flex lg:gap-4 gap-2 md:gap-4 justify-center flex">
         <div className="">
           <label className="label">
-            <span className="label-text">Email</span>
+            <span className="label-text">image</span>
           </label>
           <input
-            type="email"
-            placeholder="email"
+            type="text"
+            placeholder="image url"
             className="input lg:input-lg input-bordered lg:w-[500px] md:w-[250px] w-[150px]"
+            name = "image"
             required
           />
         </div>
         <div className="">
           <label className="label">
-            <span className="label-text">Email</span>
+            <span className="label-text">spot name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="spot name"
+            className="input lg:input-lg input-bordered lg:w-[500px] md:w-[250px] w-[150px]"
+            name = "spot_name"
+            required
+          />
+        </div>
+        </div>
+        {/* 2nd input */}
+        <div className="lg:flex md:flex lg:gap-4 gap-2 md:gap-4 justify-center flex">
+        <div className="">
+          <label className="label">
+            <span className="label-text">Country_name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Country_name"
+            className="input lg:input-lg input-bordered lg:w-[500px] md:w-[250px] w-[150px]"
+            name = "country_Name"
+            required
+          />
+        </div>
+        <div className="">
+          <label className="label">
+            <span className="label-text">location</span>
+          </label>
+          <input
+            type="text"
+            placeholder="location"
+            className="input lg:input-lg input-bordered lg:w-[500px] md:w-[250px] w-[150px]"
+            name = "location"
+            required
+          />
+        </div>
+        </div>
+        {/* 3rd input */}
+        <div className="lg:flex md:flex lg:gap-4 gap-2 md:gap-4 justify-center flex">
+        <div className="">
+          <label className="label">
+            <span className="label-text">Short description</span>
+          </label>
+          <input
+            type="text"
+            placeholder="short description"
+            className="input lg:input-lg input-bordered lg:w-[500px] md:w-[250px] w-[150px]"
+            name = "short_description"
+            required
+          />
+        </div>
+        <div className="">
+          <label className="label">
+            <span className="label-text">average_cost</span>
+          </label>
+          <input
+            type="text"
+            placeholder="average_cost"
+            className="input lg:input-lg input-bordered lg:w-[500px] md:w-[250px] w-[150px]"
+            name = "average_cost"
+            required
+          />
+        </div>
+        </div>
+        {/* 4th input */}
+        <div className="lg:flex md:flex lg:gap-4 gap-2 md:gap-4 justify-center flex">
+        <div className="">
+          <label className="label">
+            <span className="label-text">seasonality</span>
+          </label>
+          <input
+            type="text"
+            placeholder="seasonality"
+            className="input lg:input-lg input-bordered lg:w-[500px] md:w-[250px] w-[150px]"
+            name = "seasonality"
+            required
+          />
+        </div>
+        <div className="">
+          <label className="label">
+            <span className="label-text">average_cost</span>
+          </label>
+          <input
+            type="number"
+            placeholder="travel_time"
+            className="input lg:input-lg input-bordered lg:w-[500px] md:w-[250px] w-[150px]"
+            name = "travel_time"
+            required
+          />
+        </div>
+        </div>
+        {/* 5th input */}
+        <div className="lg:flex md:flex lg:gap-4 gap-2 md:gap-4 justify-center flex">
+        <div className="">
+          <label className="label">
+            <span className="label-text">totaVisitorsPerYear </span>
+          </label>
+          <input
+            type="number"
+            placeholder="seasonality"
+            className="input lg:input-lg input-bordered lg:w-[500px] md:w-[250px] w-[150px]"
+            name = "totaVisitorsPerYear "
+            required
+          />
+        </div>
+        <div className="">
+          <label className="label">
+            <span className="label-text">User Email</span>
           </label>
           <input
             type="email"
-            placeholder="email"
+            placeholder="User Email"
             className="input lg:input-lg input-bordered lg:w-[500px] md:w-[250px] w-[150px]"
+            name = "User Email"
+            required
+          />
+        </div>
+        </div>
+        {/* 6th input */}
+        <div className="flex md:ml-[90px]">
+        <div className="lg:ml-[125px] ml-[30px] md:ml-0">
+          <label className="label">
+            <span className="label-text">User Name </span>
+          </label>
+          <input
+            type="text"
+            placeholder="user name"
+            className="input lg:input-lg input-bordered lg:w-[500px] md:w-[250px] w-[150px]"
+            name = "name "
             required
           />
         </div>
