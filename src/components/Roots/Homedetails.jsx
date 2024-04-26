73c4ee617cb4079fb4cf1175df215b3f5c05dc73
Nafis Ahamed <nafisahamed14@gmail.com>
2024-04-26 15:36:52 +0600

    update user

--- src/components/Roots/Homedetails.jsx
+++ src/components/Roots/Homedetails.jsx
@@ -17,12 +17,37 @@ const Homedetails = ({item,setItems ,items}) => {
           })
          
     }
+    const updateButton = id =>{
+        console.log('hellow');
+        const updateuser = {email:"nafis ahamed"}
+        fetch(`http://localhost:5006/item/${id}`,{
+            method: 'PUT',
+            headers:{
+                'content-type':'application/json'
+            },
+            body: JSON.stringify(updateuser)
+          })
+          .then(res => res.json())
+          .then(data => {
+             console.log(data)
+             if(data.modifiedCount > 0){
+                alert('user updated successfully')
+             }
+             
+             location.reload()
+            
+          })
+
+        
+        
+    }
     return (
         <div>
 
             <div className="mt-2 flex gap-4">
             <h2 className="text-2xl text-blue-700">{email}</h2>
             <button onClick={()=>handleDelete(_id)} className="btn btn-danger ">X</button>
+            <button onClick={()=>updateButton(_id)} className="btn btn-danger ">U</button>
             </div>
             
         </div>
