846522d0f1a5634af0e1c1f29510e89f87103d4e
Nafis Ahamed <nafisahamed14@gmail.com>
2024-04-27 21:29:42 +0600

    swipper slider is done

--- src/components/Roots/Swipper.jsx
+++ src/components/Roots/Swipper.jsx
@@ -19,12 +19,6 @@ const Swipper = () => {
       modules={[Pagination, Navigation]}
       className="mySwiper"
     >
-      <div className='relative'>
-      <SwiperSlide> <img className='lg:w-full w-full lg:h-[550px] mt-5' src="https://i.postimg.cc/W1Cgj7DF/258976-cox.jpg" alt="" /> </SwiperSlide>
-      <div className='z-20 '>
-        <p className='text-3xl font-bold text-red-400'>hellow world</p>
-    </div>
-      </div>
       <SwiperSlide> 
        <div className='relative'>
        <img className='lg:w-full lg:h-[550px] mt-5' src="https://i.postimg.cc/FHwM52NM/Sundarban-Tiger.jpg" alt="" /> 
@@ -34,10 +28,25 @@ const Swipper = () => {
        </div>
       
       </SwiperSlide>
-      <SwiperSlide><img className='lg:w-full lg:h-[550px] mt-5' src="https://i.postimg.cc/mrj61787/photo-2024-04-08-22-28-31.jpg" alt="" /> </SwiperSlide>
-      <SwiperSlide><img className='lg:w-full lg:h-[550px] mt-5' src="https://i.postimg.cc/7PJ8dQKc/photo-2024-04-08-22-28-32.jpg" alt="" /> </SwiperSlide>
-      <SwiperSlide><img className='lg:w-full lg:h-[550px] mt-5' src="https://i.postimg.cc/zGVnFKzn/photo-2024-04-08-22-28-34.jpg" alt="" /> </SwiperSlide>
+       <SwiperSlide> 
+       <div className='relative'>
+       <img className='lg:w-full w-full lg:h-[550px] mt-5' src="https://i.postimg.cc/mrDsBQH9/Raja-Ampat-Island-Indonesia-850.jpg" alt="" /> 
+       <div className='z-20 absolute lg:top-[200px] md:top-[200px] top-[80px] lg:right-[50px] md:right-[30px] text-white font-bold text-center lg:text-2xl'>
+       <p>Raja Ampat, located in Indonesia's West Papua province, is an archipelago comprising <br /> over 1,500 small islands, cays, and shoals. <br /> Renowned for its breathtaking marine biodiversity  </p>
+       </div>
+       </div>
       
+      </SwiperSlide> 
+       <SwiperSlide> 
+       <div className='relative'>
+       <img className='lg:w-full w-full lg:h-[550px] mt-5' src="https://i.postimg.cc/W1Cgj7DF/258976-cox.jpg" alt="" /> 
+       <div className='z-20 absolute lg:top-[200px] md:top-[200px] top-[80px] lg:right-[10px] md:right-[30px] text-white font-bold text-center lg:text-2xl'>
+       <p>Cox's Bazar, situated in Bangladesh, boasts the world's longest uninterrupted sandy beach, <br /> stretching over 120 kilometers along the Bay of Bengal.  Renowned for its scenic beauty, Cox's Bazar attracts visitors with its golden sands, clear blue waters,  </p>
+       </div>
+       </div>
+      
+      </SwiperSlide> 
+            
     </Swiper>
     </div>
 
