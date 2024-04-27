9804dd13c1a5503e868c641280550bf687d9e8af
Nafis Ahamed <nafisahamed14@gmail.com>
2024-04-27 22:35:09 +0600

    countries card done

---
+++ src/components/Roots/Count.jsx
@@ -0,0 +1,44 @@
+import { NavLink } from "react-router-dom";
+import { FaDollarSign } from "react-icons/fa6";
+import { FaPeopleGroup } from "react-icons/fa6";
+import { IoIosTime } from "react-icons/io";
+import { TiWeatherPartlySunny } from "react-icons/ti";
+
+
+const Count = ({data}) => {
+    const {
+        _id,
+        image,
+        spot,
+        countries,
+        location,
+        description,
+        average_cost,
+        seasonality,
+        Travel,
+        totaVisitorsPerYear,
+        email,
+        name
+      } = data;
+    return (
+        <NavLink to ="/login">
+            <div className="card lg:w-96 md:w-96 w-72 bg-base-100 shadow-xl ml-7">
+      <figure>
+        <img
+          className="w-[300px] lg:w-full md:w-full"
+          src={image}
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+      <h2 className="card-title mt-9"><span className='flex mb-7 lg:mb-7 md:mb-7'></span> <span className="font-extralight">Country Name:</span> {countries}</h2>
+      <h2 className='font-bold flex items-center gap-2'> <span></span> description <span className="text-gray-400 font-extralight">{description.slice(0,40)}.....</span> </h2>
+    
+      </div>
+      
+    </div>
+        </NavLink>
+    );
+};
+
+export default Count;
--- src/components/Roots/Home.jsx
+++ src/components/Roots/Home.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CiLight } from "react-icons/ci";
 import { MdOutlineDarkMode } from "react-icons/md";
 import { Typewriter, Cursor, useTypewriter } from "react-simple-typewriter";
 import Swipper from "./Swipper";
 import { useLoaderData } from "react-router";
 import Torcard from "./Torcard";
+import Count from "./Count";
 
 const Home = () => {
   const loader = useLoaderData();
-    const [items, setItems] = useState(loader);
+
+  const [items, setItems] = useState(loader);
   const [light, setLight] = useState(true);
   const [typeEffect] = useTypewriter({
     words: ["light theme", "dark theme"],
@@ -16,6 +18,13 @@ const Home = () => {
     typeSpeed: 100,
     deleteSpeed: 50
   });
+
+  const array = items.filter(
+    (obj, index, self) =>
+      index === self.findIndex((t) => t.countries === obj.countries)
+  );
+  console.log("array", array);
+
   return (
     <div className={light ? "bg-white " : "bg-black text-white"}>
       <div
@@ -27,26 +36,42 @@ const Home = () => {
         ) : (
           <MdOutlineDarkMode className="text-5xl text-white"></MdOutlineDarkMode>
         )}
-
-        
-        
       </div>
-         <div className="mt-5">
-          <Swipper></Swipper>
-       </div>
+      <div className="mt-5">
+        <Swipper></Swipper>
+      </div>
 
-       <div>
-        <h1 className="lg:text-2xl md:text-2xl text-xl text-center font-bold mt-5">Tourist <span className="text-orange-400"> spot section</span> </h1>
+      <div>
+        <h1 className="lg:text-2xl md:text-2xl text-xl text-center font-bold mt-5">
+          Tourist <span className="text-orange-400"> spot section</span>{" "}
+        </h1>
 
-        <div className="mt-8 grid lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 lg:space-y-10 space-y-5 lg:ml-4 md:ml-[200px]" >
-            {
-                items.slice(0,6).map((data,index)=> <Torcard key={data._id} data={data} light={light} index={index}></Torcard> )
-            }
+        <div className="mt-8 grid lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 lg:space-y-10 space-y-5 lg:ml-4 md:ml-[200px]">
+          {items.slice(0, 6).map((data, index) => (
+            <Torcard
+              key={data._id}
+              data={data}
+              light={light}
+              index={index}
+            ></Torcard>
+          ))}
         </div>
+      </div>
 
-       </div>
-
-       
+      <div>
+        <h1 className="lg:text-2xl md:text-2xl text-xl text-center font-bold lg:mt-20 mt-10">
+          Country <span className="text-orange-400">section</span>{" "}
+          <div className="mt-8 grid lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 lg:space-y-10 space-y-5 lg:ml-4 md:ml-[200px]">
+          {array.slice(0, 6).map((data) => (
+            <Count
+              key={data._id}
+              data={data}
+              
+            ></Count>
+          ))}
+        </div>
+        </h1>
+      </div>
     </div>
   );
 };
